2f1d1bda4b26b9216e9d037e74cac14b656f8845
harshpandey <harshpandeyiitian04@gmail.com>
2026-02-27 02:55:35 +0530

    Production ready backend

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://harsh-pandey-portfolio.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
